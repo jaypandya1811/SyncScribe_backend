031afd0b81c6c690526577edfaff24d09c3a72dc
jay <pandyajay005@gmail.com>
2026-07-11 14:27:13 +0530

    fix: get-audio-result

--- app/services/audio_file_result/get_audio_file_result.py
+++ app/services/audio_file_result/get_audio_file_result.py
@@ -15,7 +15,9 @@ from app.core.logger import logger
 def get_audio_result_service(audio_file_id: int, db: Session) -> MeetingAudioFileResponse:
     try:
         audio_file = get_meeting_audio_file_repo(audio_file_id=audio_file_id, db=db)
-        if audio_file.status == "uploaded" or audio_file.status == "transcript_failed": # type: ignore
+        updated_data = None
+        current_status = audio_file.status
+        if current_status == "uploaded" or current_status == "transcript_failed": # type: ignore
             url = build_audio_file_url_service(audio_file_id=audio_file_id, db=db)
             transcription = transcribe_audio_service(url)
             if not transcription:
@@ -23,32 +25,41 @@ def get_audio_result_service(audio_file_id: int, db: Session) -> MeetingAudioFil
                 id= audio_file_id,
                 status= MeetingAudioFileStatus.TRANSCRIPT_FAILED,
                 )   
+                logger.error(f"transcription failed for audio file with id: {audio_file_id}")
                 updated_data = update_meeting_audio_file_repo(audio_file_id=audio_file_id, meeting_audio_file_update=meeting_audio_file_update, db=db)
-                return updated_data    
-            meeting_audio_file_update = MeetingAudioFileUpdate(
-                id= audio_file_id,
-                status= MeetingAudioFileStatus.TRANSCRIBED,
-                transcription= transcription.text,
-            )
-            updated_data = update_meeting_audio_file_repo(audio_file_id=audio_file_id, meeting_audio_file_update=meeting_audio_file_update, db=db)
-        elif audio_file.status == "transcribed" or audio_file.status == "summarization_failed": # type: ignore
+                return updated_data
+            else:
+                meeting_audio_file_update = MeetingAudioFileUpdate(
+                    id= audio_file_id,
+                    status= MeetingAudioFileStatus.TRANSCRIBED,
+                    transcription= transcription.text,
+                )
+                logger.info(f"transcription generated for audio file with id: {audio_file_id}, transcrption: {transcription.text}")
+                updated_data = update_meeting_audio_file_repo(audio_file_id=audio_file_id, meeting_audio_file_update=meeting_audio_file_update, db=db)
+                current_status = updated_data.status
+        if current_status == "transcribed" or current_status == "summarization_failed": # type: ignore
             summary = summarize_audio_service(audio_file.transcription) # type: ignore
             if not summary:
                 meeting_audio_file_update = MeetingAudioFileUpdate(
                 id= audio_file_id,
                 status= MeetingAudioFileStatus.SUMMARIZATION_FAILED,
-                )   
+                ) 
+                logger.error(f"summarization failed for audio file with id: {audio_file_id}")
                 updated_data = update_meeting_audio_file_repo(audio_file_id=audio_file_id, meeting_audio_file_update=meeting_audio_file_update, db=db)
-                return updated_data    
-            meeting_audio_file_update = MeetingAudioFileUpdate(
-                id= audio_file_id,
-                status= MeetingAudioFileStatus.SUMMARIZED,
-                summary=summary.summary,
-                # action_items=summary.action_items,
-            )
-            updated_data = update_meeting_audio_file_repo(audio_file_id=audio_file_id, meeting_audio_file_update=meeting_audio_file_update, db=db)
-            return updated_data
-        raise
+                return updated_data
+            else:     
+                meeting_audio_file_update = MeetingAudioFileUpdate(
+                    id= audio_file_id,
+                    status= MeetingAudioFileStatus.SUMMARIZED,
+                    summary=summary.summary,
+                    action_items=[item.model_dump() for item in summary.action_items],
+                )
+                logger.info(f"summary and action items generated for audio file with id: {audio_file_id}, summary: {summary}, action items: {[item.model_dump() for item in summary.action_items]}")
+                updated_data = update_meeting_audio_file_repo(audio_file_id=audio_file_id, meeting_audio_file_update=meeting_audio_file_update, db=db)
+            if updated_data is None:
+                logger.error(f"Unexpected audio file status: {audio_file.status} for id: {audio_file_id}")
+                raise AudioFileResultError()
+        return updated_data
     except MeetingAudioFileNotFoundError:
         raise
     except Exception as e:
--- app/services/meeting_audio_file/create_meeting_audio_file.py
+++ app/services/meeting_audio_file/create_meeting_audio_file.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 from fastapi import UploadFile
+from typing import cast
 from app.repositories.meeting_audio_file import create_meeting_audio_file_repo
 from app.db.models.meeting_audio import MeetingAudioFile
 from app.schema.meeting_audio_file import MeetingAudioFileCreate
@@ -7,11 +8,13 @@ from app.core.logger import logger
 from app.services.upload_audio.upload_audio_to_s3 import upload_audio_to_s3
 
 def create_meeting_audio_file_service(user_id: int, meeting_id: int, file: UploadFile, db: Session) -> MeetingAudioFile:
+    file_name = cast(str, file.filename)
     audio_url = upload_audio_to_s3(file, user_id, meeting_id)
     meeting_audio_file = MeetingAudioFileCreate(
         user_id=user_id,
         meeting_id=meeting_id,
         url=audio_url,
+        original_filename=file_name.lower()
     )
     meeting_audio_file_data = create_meeting_audio_file_repo(meeting_audio_file=meeting_audio_file, db=db)
     logger.info("meeting audio file inserted.")
