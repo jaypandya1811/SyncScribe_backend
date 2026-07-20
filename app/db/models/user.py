970e975b94984ebf64db8176975c30d0739ae01b
jay <pandyajay005@gmail.com>
2026-07-20 19:09:30 +0530

    fix: added columns in user table

--- app/db/models/user.py
+++ app/db/models/user.py
@@ -7,6 +7,8 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String, nullable=True)
+    last_name = Column(String, nullable=True)
     email = Column(String, unique=True, nullable=False, index=True)
     password = Column(String, nullable=False)
     is_active = Column(Boolean, default=True)
