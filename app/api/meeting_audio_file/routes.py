c425e27edebe0e10a1e62826827abf945a8cc36b
jay <pandyajay005@gmail.com>
2026-07-26 15:59:12 +0530

    feat: logout

--- app/api/meeting_audio_file/routes.py
+++ app/api/meeting_audio_file/routes.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Depends, status, File, UploadFile, Form
 from sqlalchemy.orm import Session
+from app.schema.users import UserResponse
+from app.services.auth import AuthService
 from typing import Optional
 from app.db.models.meeting_audio import MeetingAudioFileStatus
 from app.db.database import get_db
@@ -20,26 +22,26 @@ router = APIRouter(
     )
 
 def create_meeting_audio_file(
-    user_id: int = Form(...),
     meeting_id: int = Form(...),
     file: UploadFile = File(...),
-    db: Session = Depends(get_db)):
-    return create_meeting_audio_file_service(user_id=user_id, meeting_id=meeting_id, file=file, db=db)
+    db: Session = Depends(get_db),
+    current_user: UserResponse = Depends(AuthService.get_current_user_service)):
+    return create_meeting_audio_file_service(user_id=current_user.id, meeting_id=meeting_id, file=file, db=db)
 
 @router.get("/{audio_file_id}", response_model=MeetingAudioFileResponse, status_code=status.HTTP_200_OK)
 def get_audio_file_by_id(audio_file_id: int, db: Session = Depends(get_db)):
     return get_meeting_audio_file_service(audio_file_id=audio_file_id, db=db)
 
 @router.get(
-    "/audio/user/{user_id}",
+    "/audio/user",
     response_model=list[MeetingAudioFileResponse],
     status_code=status.HTTP_200_OK,
     )
 
 def get_meeting_audio_files_by_user_id(
-    user_id: int,
-    db: Session = Depends(get_db)):
-    return get_meeting_audio_file_by_user_service(user_id=user_id, db=db)
+    db: Session = Depends(get_db),
+    current_user: UserResponse = Depends(AuthService.get_current_user_service)):
+    return get_meeting_audio_file_by_user_service(user_id=current_user.id, db=db)
 
 @router.get(
     "/audio/meeting/{meeting_id}",
@@ -53,16 +55,16 @@ def get_meeting_audio_files_by_meeting_id(
     return get_meeting_audio_file_by_meeting_service(meeting_id=meeting_id, db=db)
 
 @router.get(
-    "/audio/user/{user_id}/meeting/{meeting_id}",
+    "/audio/user/meeting/{meeting_id}",
     response_model=list[MeetingAudioFileResponse],
     status_code=status.HTTP_200_OK,
     )
 
 def get_meeting_audio_files_by_user_id_and_meeting_id(
-    user_id: int,
     meeting_id: int,
-    db: Session = Depends(get_db)):
-    return get_meeting_audio_file_by_meeting_and_user_service(user_id=user_id, meeting_id=meeting_id,db=db)
+    db: Session = Depends(get_db),
+    current_user: UserResponse = Depends(AuthService.get_current_user_service)):
+    return get_meeting_audio_file_by_meeting_and_user_service(user_id=current_user.id, meeting_id=meeting_id,db=db)
 
 @router.put("/audio/update/{audio_file_id}", response_model=MeetingAudioFileResponse, status_code=status.HTTP_200_OK)
 def update_meeting_audio_file(
